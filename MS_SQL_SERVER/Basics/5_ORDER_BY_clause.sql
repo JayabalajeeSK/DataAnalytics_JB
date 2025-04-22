589cec152711c573452ad93eb5c9ad088059abee
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-04-22 10:54:08 +0530

    5. OREDR BY clause

---
+++ MS_SQL_SERVER/Basics/5_ORDER_BY_clause.sql
@@ -0,0 +1,13 @@
+select * from [dbo].[EmployeeRecords]
+
+select * from [dbo].[EmployeeRecords]
+ORDER BY Salary
+
+select * from [dbo].[EmployeeRecords1]
+order by Salary desc
+
+select * from [dbo].[EmployeeRecords1]
+order by First_name asc,Salary desc
+
+select * from [dbo].[EmployeeRecords1]
+order by department asc,Salary desc
