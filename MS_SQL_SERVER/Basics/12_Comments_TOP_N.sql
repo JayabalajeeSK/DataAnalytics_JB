cbfc48a7b05d5926176864bb8e10f8f934841c78
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-04-29 12:09:19 +0530

    Comments and TOP N

---
+++ MS_SQL_SERVER/Basics/12_Comments_TOP_N.sql
@@ -0,0 +1,18 @@
+--Hi we are learning SQL Server
+
+select * from dbo.EmployeeRecords1
+
+/*
+Hi
+we 
+are 
+learning
+sql
+server
+*/
+
+select * from dbo.EmployeeRecords1
+
+select top 2 * from dbo.EmployeeRecords1
+select top 2 employee_id,salary from dbo.EmployeeRecords1
+select TOP 5 first_name,last_name from dbo.EmployeeRecords1
